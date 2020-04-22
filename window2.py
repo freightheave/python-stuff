27a3d90719f0de9e0b5476e516be67ab1a04283f
Roshan Komaravolu <komaravolu.roshan@gmail.com>
2020-04-23 00:54:59 +0530

    Messing around with tkinter lib
    random stuff

--- window2.py
+++ window2.py
@@ -1,25 +1,25 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
 
-class Application(tk.Frame):
+class Window(Frame):
     def __init__(self, master=None):
-        super().__init__(master)
+        Frame.__init__(self, master)
         self.master = master
-        self.pack()
-        self.create_widgets()
+        self.pack(fill=BOTH, expand=1)
+        
+        load = Image.open("chess_sprite.png")
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=0, y=0)
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        
+root = Tk()
+app = Window(root)
+app.configure(bg='white')
+root.wm_title("Chess Sprites")
+root.geometry("256x85")
+root.mainloop()
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+input()
